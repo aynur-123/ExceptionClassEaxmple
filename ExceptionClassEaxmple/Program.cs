fc99d7b15b6bcb7eb2d15c51920923c7754dff40
AYNUR <AYNUR@DESKTOP-AQDSSFG>
2024-08-30 16:57:31 +0300

    Proje dosyası ekle.

---
+++ ExceptionClassEaxmple/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExceptionClassEaxmple;
+
+class Program
+{ 
+    static void Main(string[] args)
+    {
+		try
+		{
+			Find();
+		}
+		catch (RecordNotFoundException exception)
+		{
+			Console.WriteLine(exception.Message);
+		}
+		Console.ReadLine();
+    }
+
+
+private static void Find()
+{
+    List<string> students = new List<string> { "Elif","Derin","Serap"};
+    if (!students.Contains("Ahmet"))
+    { throw new RecordNotFoundException("Record Not Found!!"); }
+
+}
+
+}
---
+++ ExceptionClassEaxmple/RecordNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionClassEaxmple
+{
+    public class RecordNotFoundException:Exception
+    {
+        public RecordNotFoundException(string message):base(message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+}
